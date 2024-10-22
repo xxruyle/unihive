c056d7b13d1233f0ca4d14eaae0218d62fd29b4d
xxruyle <xrcnerd@gmail.com>
2024-10-21 23:12:48 -0500

    add create university and course front end behavior

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -11,8 +11,13 @@ def home():
 def university(university=None):
     return render_template('university_home.html', uni=university) 
 
-@app.route("/u/create-university")
+@app.route("/create-university", methods =["GET", "POST"])
 def create_university():
+    if request.method == "POST": 
+        university = request.form.get('university')
+        university_acro = request.form.get('university-acro')
+        return redirect(url_for('university', university=university_acro))
+
     return render_template('create_university.html') 
 
 @app.route("/u")
@@ -21,8 +26,14 @@ def course(university=None, course=None):
     return render_template('course.html', uni=university, course=course) 
 
 
-@app.route("/u")
-@app.route("/u/<university>/create-course")
-def create_course(university=None): 
+@app.route("/create-course", methods=["GET", "POST"])
+def create_course(university="placeholder"): 
+    if request.method == "POST": 
+        university = request.form.get('university')
+        department = request.form.get('department')
+        course_number = request.form.get('course-number')
+        return redirect(url_for('course', university=university, course=f"{department}-{course_number}"))
+
+
     return render_template('create_course.html', uni=university) 
 
