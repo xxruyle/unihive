fe07d0b5caa2d7d9df13fbd856a138f314a7d6e7
Andrew Ward <143913159+a472w679@users.noreply.github.com>
2024-10-24 12:23:42 -0500

    Add initial database logic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from university import *
 from course import * 
+import db
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
